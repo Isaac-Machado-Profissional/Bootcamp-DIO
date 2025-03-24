4386260415e90e600052fb5613a88e5d704e9934
Isaac Machado <isaacmachado.profissional@gmail.com>
2025-03-24 20:14:59 -0300

    Spring Security, Health Check

---
+++ 2-Modulo/Development-APIs-Spring-Framework/4_Arquitetura-Aplicacoes-Rest-Spring-WEB/4_4-JUnit/Junit/REST-Spring/src/test/java/br/com/dio/ProdutoTest.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ProdutoTest {
+
+	@Test // Anotação pra rodar JUnit Test
+	public void testeFalso() {
+
+		assertTrue(true); // assert verfiica se true
+
+	}
+
+}
